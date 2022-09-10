4f77c21eb84ca9b5eee49cfa81c4ac1738708085
Phan Nguyen <phananh.nguyen.vn@gmail.com>
2022-09-10 17:12:12 -0400

    update

--- QANT530/discussion2.r
+++ QANT530/discussion2.r
@@ -1,4 +1,12 @@
 library(xlsx)
 library(olsrr)
 data <- read.xlsx("QANT530/datasets/Practice Dataset (2).xlsx", 1)
-data
+head(data)
+
+model <- lm(AverageMonthlyOrders ~ factor(Region) + factor(WarehouseType) + NumberOfEmployees + AverageSalary + InventoryValue, data=data )
+fwsel <- ols_step_forward_aic(model,penter=.05)
+fwsel
+
+model <- lm(AverageMonthlyOrders~NumberOfEmployees + factor(Region),data=data)
+summary(model)
+ 
